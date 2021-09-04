1cd68bce1895949e9bc628fb049f8514e8fa63b1
Nicholas Chin <njc94@scarletmail.rutgers.edu>
2021-09-03 20:42:32 -0400

    added get request

--- server.js
+++ server.js
@@ -16,9 +16,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // Get Route for notes
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/public/notes.html")));
+app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "/public/notes.html")));
 
 // Get Route for index file
 app.get("*", (req, res) => res.sendFile(path.join(__dirname, "/public/index.html")));
+
+// GET Request for all saved notes
+app.get("/api/notes", (req, res) => {
+  res.status(200).json(`${req.method} request received to get notes`);
+  // Log request to the terminal
+  console.log(`${req.method} request received to get notes`);
+  // Returns all saved notes as JSON
+  return res.json(notes);
+});
 
 app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}🚀`));
