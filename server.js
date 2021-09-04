fbc1a900908a7ecbd075f510821c8c0e0b05ffba
Nicholas Chin <njc94@scarletmail.rutgers.edu>
2021-09-03 21:04:29 -0400

    added post request. Now debugging

--- server.js
+++ server.js
@@ -2,12 +2,15 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+var notes = require("./db/db.json");
 
 // Initiate Express
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express);
+
+currentID = notes.length;
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
@@ -24,10 +27,27 @@ app.get("*", (req, res) => res.sendFile(path.join(__dirname, "/public/index.html
 // GET Request for all saved notes
 app.get("/api/notes", (req, res) => {
   res.status(200).json(`${req.method} request received to get notes`);
-  // Log request to the terminal
+  // Log GET request to the terminal
   console.log(`${req.method} request received to get notes`);
   // Returns all saved notes as JSON
   return res.json(notes);
 });
 
-app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}🚀`));
+//POST request
+app.post("/api/notes", (req, res) => {
+  // Log that a POST request whas received
+  console.info(`${req.method} request received to add a note`);
+  let newNote = req.body;
+  newNote.id = currentID + 1;
+  console.log(newNote);
+  // Add a new note
+  notes.push(newNotes);
+
+  const response = {
+    status: "success",
+    body: newNote,
+  };
+  console.log(response);
+  res.status(201).json(response);
+});
+app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
